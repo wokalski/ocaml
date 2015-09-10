04cd20e0af63e25d0e99a9583d43d4568d0bb9c4
Gerd Stolpmann <gerd@gerd-stolpmann.de>
2015-09-10 18:11:25 +0200

    ios: update SDK version (8.4). Remove -no-pthread.

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ arch=armv7
 # valid for phone: armv6, armv7, armv7s, arm64
 # valid for simulator: i386, x86_64
 
-sdk=8.3
+sdk=8.4
 
 
 export PLT="/Applications/Xcode.app/Contents/Developer/Platforms/${platform}.platform"
@@ -20,7 +20,7 @@ export SDK="/Developer/SDKs/${platform}${sdk}.sdk"
         -as "gcc -arch $arch -isysroot $PLT$SDK -miphoneos-version-min=$sdk -c" \
         -aspp "gcc -arch $arch -isysroot $PLT$SDK -miphoneos-version-min=$sdk -c" \
         -libs "-Wl,-syslibroot,$PLT$SDK" \
-        -no-curses -no-pthread -no-shared-libs \
+        -no-curses -no-shared-libs \
         -no-debugger -no-ocamldoc -no-ocamlbuild -no-graph \
         -target $arch-apple-darwin \
         "$@" || exit
